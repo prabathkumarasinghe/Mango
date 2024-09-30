4c3f85a4a8f69bbac647441af5c78b6784ef0377
pk <kpmprabath@gmail.com>
2024-09-30 19:17:50 +0930

    Update AuthAPI

---
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,50 @@
+﻿using Azure;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        protected ResponseDto _responseDto;
+
+        public AuthAPIController(IAuthService authService)
+        {
+            _authService = authService;
+            _responseDto = new();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterationRequestDto model) 
+        {
+            var errorMessage = await _authService.Register(model);
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message= errorMessage;
+                return BadRequest(_responseDto);
+            }
+            
+                return Ok(_responseDto);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model )
+        {
+            var loginResponse = await _authService.Login(model);
+            if (loginResponse.User == null)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = "Username or password is incorrect";
+                return BadRequest(_responseDto);
+            }
+            _responseDto.Result = loginResponse;
+            return Ok(_responseDto);
+        }
+    }
+}
---
+++ Mango.Services.AuthAPI/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class LoginResponseDto
+    {
+        public string User { get; set; }
+        public string Token { get; set; }
+    }
+}
